8529987d95bb75d2942e1b4205d2ff5215b32283
zayed khan <zayedk458@gmail.com>
2025-09-30 18:33:55 +0600

    prepend method

--- week-1/LinkdList-1.py
+++ week-1/LinkdList-1.py
@@ -63,4 +63,14 @@ def pop_method(self):
     return temp.value #.value is for value(Testing purpose)
         
 
- 
+#prepend method
+def prepend(self,value):
+    new_node=Node(value)
+    if self.length==0:
+        self.head=new_node
+        self.tail=new_node
+    else:
+        new_node.next=self.head
+        self.head=new_node
+    self.length +=1
+    return True #optional
